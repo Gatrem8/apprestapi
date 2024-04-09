8c728fd41ee67904d5c406eead402705bfcef3b2
Atang Sukmana <atangsukmana7@gmail.com>
2024-04-09 12:04:10 +0700

    Berhasil menampilkan semua mahasiswa by ID

--- controller.js
+++ controller.js
@@ -17,3 +17,19 @@ exports.tampilSemuaMahasiswa = function (req, res) {
     }
   });
 };
+
+//Menampilkan semua data mahasiswa berdasarkan id
+exports.tampilSemuaMahasiswaById = function (req, res) {
+  let id = req.params.id;
+  connection.query(
+    "SELECT * FROM mahasiswa WHERE id_mahasiswa = ?",
+    [id],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
