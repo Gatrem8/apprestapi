f8f3ce0f3cd1985370f2dcf9e10ed1738965842f
Atang Sukmana <atangsukmana7@gmail.com>
2024-04-10 12:29:18 +0700

    Berhasil menampilkan matakuliah

--- controller.js
+++ controller.js
@@ -89,3 +89,17 @@ exports.hapusMahasiswa = function (req, res) {
     }
   );
 };
+
+//Menampilkan matakuliah group
+exports.tampilGroupMatakuliah = function (req, res) {
+  connection.query(
+    "SELECT mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, matakuliah.matakuliah, matakuliah.sks FROM krs JOIN mahasiswa JOIN matakuliah WHERE krs.id_matakuliah = matakuliah.id_matakuliah AND krs.id_mahasiswa = mahasiswa.id_mahasiswa ORDER BY mahasiswa.id_mahasiswa",
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.oknested(rows, res);
+      }
+    }
+  );
+};
--- res.js
+++ res.js
@@ -8,3 +8,35 @@ exports.ok = function (values, res) {
   res.json(data);
   res.end();
 };
+
+exports.oknested = function (values, res) {
+  const hasil = values.reduce((akumulasikan, item) => {
+    if (akumulasikan[item.nama]) {
+      const group = akumulasikan[item.nama];
+      // Periksa apakah group.matakuliah sudah ada dan berisi array
+      if (group.matakuliah && Array.isArray(group.matakuliah)) {
+        group.matakuliah.push(item.matakuliah);
+      } else {
+        // Jika group.matakuliah belum ada atau tidak berisi array, buat array baru
+        group.matakuliah = [group.matakuliah, item.matakuliah].filter(Boolean);
+      }
+    } else {
+      // Buat group baru jika belum ada
+      akumulasikan[item.nama] = {
+        nim: item.nim,
+        nama: item.nama,
+        jurusan: item.jurusan,
+        // Buat array matakuliah dan tambahkan matakuliah pertama
+        matakuliah: [item.matakuliah],
+      };
+    }
+    return akumulasikan;
+  }, {});
+
+  var data = {
+    status: 200,
+    values: hasil,
+  };
+  res.json(data);
+  res.end();
+};
