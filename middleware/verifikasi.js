0ae4696c6fddd772783b84e3de0e43714f6850a7
Atang Sukmana <atangsukmana7@gmail.com>
2024-04-13 17:13:23 +0700

    role beres

---
+++ middleware/verifikasi.js
@@ -0,0 +1,72 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/secret");
+
+function verifikasi(role) {
+  return function (req, res, next) {
+    var role = req.body.role;
+    // Check Authorization header
+    var tokenWithBearer = req.headers.authorization;
+    if (tokenWithBearer) {
+      var token = tokenWithBearer.split(" ")[1];
+
+      // Verification
+      jwt.verify(token, config.secret, function (err, decoded) {
+        if (err) {
+          return res
+            .status(401)
+            .send({ auth: false, message: "Token tidak terdaftar!" });
+        } else {
+          if (role == 2) {
+            req.auth = decoded;
+            next();
+          } else {
+            return res
+              .status(401)
+              .send({ auth: false, message: "Gagal mengotorisasi role anda!" });
+          }
+        }
+      });
+    } else {
+      return res
+        .status(401)
+        .send({ auth: false, message: "Token tidak tersedia!" });
+    }
+  };
+}
+
+module.exports = verifikasi;
+
+// function verifikasi() {
+//   return function (req, res, next) {
+//     var role = req.body.role;
+//     //check authorizzation header
+//     var tokenWithBearer = req.headers.authorizzation;
+//     if (tokenWithBearer) {
+//       var token = tokenWithBearer.split(" ")[1];
+
+//       //verifikasi
+//       jwt.verify(token, config.secret, function (err, decoded) {
+//         if (err) {
+//           return res
+//             .status(401)
+//             .send({ auth: false, message: "Token tidak terdaftar!" });
+//         } else {
+//           if (role == 2) {
+//             req.auth = decoded;
+//             next();
+//           } else {
+//             return res
+//               .status(401)
+//               .send({ auth: false, message: "Gagal mengotorisasi role anda!" });
+//           }
+//         }
+//       });
+//     } else {
+//       return res
+//         .status(401)
+//         .send({ auth: false, message: "Token tidak tersedia!" });
+//     }
+//   };
+// }
+
+// module.exports = verifikasi;
